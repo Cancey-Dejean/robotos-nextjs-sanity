ba1b40b7b6c0cf40c161b968fd7252de365494e5
Cancey-Dejean <you@example.com>
2024-07-14 22:35:55 -0500

    Updated Styles for newsletter

--- components/Newsletter/SubscribeForm/index.tsx
+++ components/Newsletter/SubscribeForm/index.tsx
@@ -12,6 +12,7 @@ const SubscribeForm = () => {
   const [successMessage, setSuccessMessage] =
     useState<MembersSuccessResponse>();
   const [errorMessage, setErrorMessage] = useState("");
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -28,28 +29,26 @@ const SubscribeForm = () => {
 
     if (data.error) {
       setErrorMessage("Hey, you're already subscribed!");
-
       setSuccessMessage(undefined);
       return;
     }
 
     setSuccessMessage(data.res);
     setErrorMessage("");
+    setInput(""); // Clear the input field
   };
-
-  console.log(errorMessage);
 
   const dismissMessages = () => {
     setSuccessMessage(undefined);
     setErrorMessage("");
   };
+
   return (
     <section className="py-20">
       <div className="mb-8 text-center">
         <SectionTitle as="h2" className="mb-4" size="medium">
           Subscribe
         </SectionTitle>
-
         <p>Get notified when we launch.</p>
       </div>
 
@@ -65,7 +64,6 @@ const SubscribeForm = () => {
           type="email"
           className="min-h-[88px] text-2xl"
         />
-        {/* <button type="submit">Subscribe</button> */}
         <Button className="bg-pink-200 px-4 text-white" type="submit">
           Subscribe
         </Button>
@@ -73,16 +71,16 @@ const SubscribeForm = () => {
 
       <div className="relative mx-auto mt-5 w-full max-w-lg">
         {(successMessage || errorMessage) && (
-          <div className="flex items-start space-x-2 rounded-[12px] bg-purple-100 px-2 py-4">
+          <div className="flex items-start space-x-2 rounded-[12px] bg-[#f3f3f3] px-2 py-4">
             <div>
               <CheckCircle className="size-4" />
             </div>
 
-            <div className="text-xs text-white sm:text-sm">
+            <div className="text-xs text-black sm:text-sm">
               {successMessage ? (
                 <p>
                   We&apos;ve added{" "}
-                  <span className="font-bold text-blue-200">
+                  <span className="font-bold italic text-purple-200">
                     {successMessage.email_address}
                   </span>{" "}
                   to our waitlist. We&apos;ll let you know when we launch!
